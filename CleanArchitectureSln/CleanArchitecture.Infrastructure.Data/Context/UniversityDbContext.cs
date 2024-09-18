4c90aab6adc55016dd03a074493a25d0f370f2e8
MonetteParangat <monette.parangat@ibm.com>
2024-09-18 21:13:41 +0800

    Added UniversityDbContext and CourseRepository on Infrastructure.Data

---
+++ CleanArchitectureSln/CleanArchitecture.Infrastructure.Data/Context/UniversityDbContext.cs
@@ -0,0 +1,17 @@
+﻿using CleanArchitecture.Domain.Models;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CleanArchitecture.Infrastructure.Data.Context
+{
+    public class UniversityDbContext : DbContext
+    {
+        public UniversityDbContext(DbContextOptions options) : base(options) { }
+
+        public DbSet<Course> Courses { get; set; }
+    }
+}
---
+++ CleanArchitectureSln/CleanArchitecture.Infrastructure.Data/Repository/CourseRepository.cs
@@ -0,0 +1,25 @@
+﻿using CleanArchitecture.Domain.Interfaces;
+using CleanArchitecture.Domain.Models;
+using CleanArchitecture.Infrastructure.Data.Context;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CleanArchitecture.Infrastructure.Data.Repository
+{
+    public class CourseRepository : ICourseRepository
+    {
+        private UniversityDbContext _context;
+        public CourseRepository(UniversityDbContext context) 
+        { 
+            _context = context;
+        }
+
+        public IEnumerable<Course> GetCourses()
+        {
+           return _context.Courses;
+        }
+    }
+}
