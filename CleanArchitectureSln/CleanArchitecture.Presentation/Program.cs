cd8291c5f98115812f1a3aac5223a02b47654998
MonetteParangat <monette.parangat@ibm.com>
2024-09-18 20:57:32 +0800

    set ignIn.RequireConfirmedAccount = false in program file

--- CleanArchitectureSln/CleanArchitecture.Presentation/Program.cs
+++ CleanArchitectureSln/CleanArchitecture.Presentation/Program.cs
@@ -13,7 +13,7 @@ builder.Services.AddDbContext<ApplicationDbContext>(options =>
     options.UseMySql(universityIdentityConnectionString, ServerVersion.AutoDetect(universityIdentityConnectionString)));
 builder.Services.AddDatabaseDeveloperPageExceptionFilter();
 
-builder.Services.AddDefaultIdentity<IdentityUser>(options => options.SignIn.RequireConfirmedAccount = true)
+builder.Services.AddDefaultIdentity<IdentityUser>(options => options.SignIn.RequireConfirmedAccount = false)
     .AddEntityFrameworkStores<ApplicationDbContext>();
 builder.Services.AddControllersWithViews();
 
