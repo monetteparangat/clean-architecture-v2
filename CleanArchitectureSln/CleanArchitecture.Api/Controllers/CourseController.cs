66f9cc48d83fc65da0fcd16f64587794a6c4fbb9
MonetteParangat <monette.parangat@ibm.com>
2024-09-19 23:54:20 +0800

    Inital comit for CleanArchitecture.Api and added Courses in Model

---
+++ CleanArchitectureSln/CleanArchitecture.Api/Controllers/CourseController.cs
@@ -0,0 +1,20 @@
+using Microsoft.AspNetCore.Mvc;
+
+namespace CleanArchitecture.Api.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class CourseController : ControllerBase
+    {
+        private readonly ILogger<CourseController> _logger;
+
+        public CourseController(ILogger<CourseController> logger)
+        {
+            _logger = logger;
+        }
+
+
+
+
+    }
+}
--- CleanArchitectureSln/CleanArchitecture.Domain/Models/Course.cs
+++ CleanArchitectureSln/CleanArchitecture.Domain/Models/Course.cs
@@ -12,5 +12,7 @@ namespace CleanArchitecture.Domain.Models
         public string? Name { get; set; }
         public string? Description { get; set; }
         public string? ImageUrl { get; set; }
+
+        public IEnumerable<Course> Courses { get; set; }
     }
 }
