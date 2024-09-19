c14c340eda3cfe0b80c15a012ba678e04b04c36d
MonetteParangat <monette.parangat@ibm.com>
2024-09-19 15:05:12 +0800

    Implement MVC Course at Presentation layer

---
+++ CleanArchitectureSln/CleanArchitecture.Presentation/Controllers/CourseController.cs
@@ -0,0 +1,23 @@
+﻿using CleanArchitecture.Application.Interfaces;
+using CleanArchitecture.Application.ViewModels;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+
+namespace CleanArchitecture.Presentation.Controllers
+{
+    [Authorize]
+    public class CourseController : Controller
+    {
+        private ICourseService _courseService;
+
+        public CourseController(ICourseService courseService)
+        {
+            _courseService = courseService;
+        }
+        public IActionResult Index()
+        {
+            CourseVM model = _courseService.GetCourses();
+            return View(model);
+        }
+    }
+}
