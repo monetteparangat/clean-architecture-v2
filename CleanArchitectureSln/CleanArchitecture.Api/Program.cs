6cb039384c9ab7abf0a424e75933d1c1a8d86421
MonetteParangat <monette.parangat@ibm.com>
2024-09-20 00:06:02 +0800

    Added mysql connection program.cs file and appsettings for Api layer

--- CleanArchitectureSln/CleanArchitecture.Api/Program.cs
+++ CleanArchitectureSln/CleanArchitecture.Api/Program.cs
@@ -1,3 +1,7 @@
+using CleanArchitecture.Infrastructure.Data.Context;
+using CleanArchitecture.Infrastructure.IoC;
+using Microsoft.EntityFrameworkCore;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
@@ -6,6 +10,15 @@ builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
+
+//AddDbContext
+var universityDbConnectionString = builder.Configuration.GetConnectionString("UniversityDbConnection") ?? throw new InvalidOperationException("Connection String 'UniversityDbConnection' not found");
+
+builder.Services.AddDbContext<UniversityDbContext>(options =>
+    options.UseMySql(universityDbConnectionString, ServerVersion.AutoDetect(universityDbConnectionString)));
+
+//Register services
+DependencyContainer.RegisterServices(builder.Services);
 
 var app = builder.Build();
 
